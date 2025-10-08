91ad031251840e3c45d7c107f6c5e72010dfb588
Yusuf Ehab <145431804+YusufEhab@users.noreply.github.com>
2025-10-08 23:34:47 +0300

    Update isBipartite.cpp

--- Templates/Graphs/isBipartite.cpp
+++ Templates/Graphs/isBipartite.cpp
@@ -1,18 +1,18 @@
 vector<vector<int>> adj;
-vector<int> col;
+vector<int> col(n+1, -1);
 
 bool isBipartite() {
     bool isbipartite = 1;
     for (int i = 1; i <= n; ++i) {
-        if (col[i] == -1) {
-            col[i] = 0;
+        if (col[i] == -1) { //if not colored 
+            col[i] = 0; // color it with 0
             queue<int> q;
             q.push(i);
-            while (!q.empty()) {
+            while (!q.empty()) { // traverse all its path 
                 int p = q.front();
                 q.pop();
-                for (auto j: adj[p]) {
-                    if (col[j] == -1) {
+                for (auto j: adj[p]) { // check the neighbours
+                    if (col[j] == -1) { // if not colored make it inverse color and push
                         col[j] = col[p] ^ 1;
                         q.push(j);
                     } else {
