6e13e721797def045955c51f473a7ca98856440f
Yusuf Ehab <145431804+YusufEhab@users.noreply.github.com>
2026-03-20 19:33:05 +0200

    Refactor DFS implementation and add graph algorithms

--- Templates/Graphs/dfs.cpp
+++ Templates/Graphs/dfs.cpp
@@ -1,29 +1,69 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-const int N = 100+20, M = 100+20;
-vector<vector<int>> adj[N];
-bool vis[N];
-int n, m, cnt;
-
-void dfs(int v){
-	vis[v] = true;
-	++cnt;
-	for(auto &u: adj[v])
-		if(!vis[u]) dfs(u);
-
+//Make sure to apply the operation on all components 
+//traverse on connected component
+void dfs(int u) {
+    vis[u] = 1;
+    for (auto &v: adj[u]) {
+        if (vis[v]) continue;
+        dfs(v);
+    }
 }
-signed main(){
-	ios::sync_with_stdio(false),cin.tie(nullptr);
-	
-	cin >> n >> m;
-	for(int i = 0; i < m; ++i){
-		int u, v;
-		cin >> u >> v;
-		adj[u].emplace_back(v);
-		adj[v].emplace_back(u);
-	}
-	dfs(1);
-	if (cnt == n) cout << "connected\n";
-	else cout << "NotConnected\n";
+//Size of component
+int dfs(int u) {
+    vis[u] = 1;
+    int ans = 1;
+    for (auto &v: adj[u]) {
+        if (vis[v]) continue;
+        ans += dfs(v);
+    }
+    return ans;
+}
+//Cycle detection undirected
+bool cycle(int u, int par) {
+    vis[u] = 1;
+    for (auto &v: adj[u]) {
+        if (v == par) continue;
+        if (vis[v]) return true;
+        if (dfs(v, u)) return true;
+    }
+    return false;
+}
+//Cycle detection directed
+int col[N];
+int cycle(int u) {
+    col[u] = 1;
+    for (auto &v: adj[u]) {
+        if (col[u] == 0) {
+            if (dfs(v)) return true;
+        }
+        else if (col[u] == 1) return true;
+    }
+    col[u] = 2;
+    return false;
+}
+//Bipartite component
+int col[N];
+bool bi(int u, int par) {
+    for (auto &v: adj[u]) {
+        if (v == par) continue;
+        if (col[u] == 0) {
+            col[v] = 3 - col[u];
+            if(!bi(v, u)) return false;
+        }else if (col[v] == col[u]) return false;
+    }
+    return true;
+}
+
+// topological sort (reversed)
+int col[N], ans[N], sz;
+bool topsort(int u) {
+    col[u] = 1;
+    for (auto &v: adj[u]) {
+        if (col[u] == 0) {
+            if (dfs(v)) return true;
+        }
+        else if (col[u] == 1) return true;
+    }
+    col[u] = 2;
+    ans[sz++] = u;
+    return false;
 }
