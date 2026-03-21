1947d921a4b235d9b10f69436849dd72b25ec09d
Yusuf Ehab <145431804+YusufEhab@users.noreply.github.com>
2026-03-21 11:12:02 +0200

    Implement Parity Disjoint Set Union (DSU) structure

--- Templates/DataStructure/ParityDSU.cpp
+++ Templates/DataStructure/ParityDSU.cpp
@@ -1,1 +1,27 @@
 //Bipartite checking
+const int N = 2e5+5;
+struct DSU {
+    vector<int> par;
+    int cmp;
+    int col[N];
+    DSU(int n) {
+        par.assign(n+1, -1);
+        cmp = n;
+    }
+    pair<int,int> find(int u) {
+        if (par[u] < 0) return {u,0};
+        auto [r,d] = find(par[u]);
+        col[u] ^= col[par[u]];
+        par[u] = r;
+        return {r,col[u]};
+    }
+    int merge(int s, int b, bool f) {
+        auto [rs, ds] = find(s);
+        auto [rb, db] = find(b);
+        if (rs == rb) return (ds^db) == f? 1 : -1;
+        if (par[rb] > par[rs]) swap(rs,rb);
+        par[rs] = b;
+        col[rs] = ds^db^f;
+        return 0;
+    }
+};
